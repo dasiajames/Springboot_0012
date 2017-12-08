b3958f7e2a783e640763c14d415ca3e7e74f7dff
Dasia James <dasiajames@Dasias-MacBook-Air.local>
2017-12-08 10:57:07 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Springboot0012Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot0012Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot0012Application.class, args);
+	}
+}
